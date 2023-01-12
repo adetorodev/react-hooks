ed9409cdec9fffbee085e28b52f619e3ec84fb39
Ezekiel Adetoro <adetoroe787@gmail.com>
2023-01-12 15:28:14 +0100

    task: useReducer with useContext

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from "./logo.svg";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
 import Form from "./Component/ArtWork";
 import Person from "./Component/ClassCounter";
 import SiteHeader from "./Component/useffHook";
@@ -13,25 +13,49 @@ import MouseContainer from "./useEffectHooks/MouseContainer";
 import IntervalClassCounter from "./useEffectHooks/IntervalClassCounter";
 import IntervalHookCounter from "./useEffectHooks/IntervalHookCounter";
 import DataFetching from "./useEffectHooks/DataFetching";
-import ComponentC from "./useContext/ComponentC";
+import ComponentC from "./Reducer/ComponentC";
 import CounterOne from "./Reducer/CounterOne";
 import CounterTwo from "./Reducer/CounterTwo";
+import ComponentA from "./Reducer/ComponentA";
+import ComponentB from "./Reducer/ComponentB";
 // import { Nav, Title } from "./Component/book/Chapt1";
 
-const menus = [
-  { key: "home", label: "Home" },
-  { key: "product", label: "Product" },
-  { key: "about", label: "About" },
-  { key: "secure", label: "Secure", disabled: true },
-];
+// const menus = [
+//   { key: "home", label: "Home" },
+//   { key: "product", label: "Product" },
+//   { key: "about", label: "About" },
+//   { key: "secure", label: "Secure", disabled: true },
+// ];
 
-export const UserContext = React.createContext();
-export const ChannelContext = React.createContext();
+// export const UserContext = React.createContext();
+// export const ChannelContext = React.createContext();
+export const CountContext = React.createContext()
 
+const initialState = 0
+const reducer = (state, action) => {
+    switch (action) {
+        case 'increment':
+            return state + 1
+        case 'decrement':
+            return state - 1
+        case 'reset':
+            return initialState
+        default:
+            return state
+    }
+}
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState)
   return (
+    
     <div className="App">
-      <CounterTwo />
+      <CountContext.Provider value={({ countState: count, countDispatch: dispatch})}>
+      Count - {count}
+      <ComponentA />
+      <ComponentB />
+      <ComponentC />
+      </CountContext.Provider>
+      {/* <CounterTwo /> */}
       {/* <CounterOne /> */}
       {/* <UserContext.Provider value={"Ezekiel"}>
         <ChannelContext.Provider value={'ZestDev'}>
